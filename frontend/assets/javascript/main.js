32f00ea1217a8a41b6629a04bfae899d803c5374
Jarrett Huang <jarethuang@gmail.com>
2019-03-22 16:29:53 -0700

    Updated main.js
    Cleaned up front end scripts

--- frontend/assets/javascript/main.js
+++ frontend/assets/javascript/main.js
@@ -1,64 +1,76 @@
 
-$(document).ready(function(){
-    var random_boolean = Math.random() >= 0.5;
+
+$(runApp);
+let isDarkMode = true;
+
+function runApp() {
+    sendInitialQuery();
+    randomizeTheme();
+
+}
+
+function randomizeTheme() {
+    let random_boolean = Math.random() >= 0.5;
     if (random_boolean) {
-        $("body").css("background-color", "white");
-        $("body").css("filter", "invert(100%)");
+        let body = $("body");
+        body.css("background-color", "white");
+        body.css("filter", "invert(100%)");
+        isDarkMode = false;
     }
-    
-    $("#join-button").click(function() {
-        $(".submit-text").show();
-        $(".check-submit").hide();
-        $("#application-form").modal({
-            fadeDuration: 100
-        });
-    });
-    
-    $("#ign-input").focusout(function() {
-        jsonParser($("#ign-input").val());
-    });
-    
-    var previousLookupName = "";
-    
-    function jsonParser(name) {
-        if (name == "" || name == previousLookupName) return;
-        var url = "https://api.silveress.ie/bns/v3/character/full/na/" + name;
-        previousLookupName = name;
-        showSpinner();
-    
-        $.getJSON(url, function(data) {
-            hideSpinner();
-            if (isEmpty(data)) {
-                $("#char-lookup").html("Cannot find you on NA server!");
-            } else {
-                $("#char-lookup").html(confirmationText(data));
-            }
-            showCharacterLookup();
-        });
-    }
-    
-    function showCharacterLookup() {
-        $("#char-lookup").show();
-    }
-    
-    function isEmpty(obj) {
-        for(var key in obj) {
-            if(obj.hasOwnProperty(key))
-            return false;
+}
+
+function sendInitialQuery() {
+
+// TODO
+}
+
+function sendQuery(query) {
+    return new Promise(function(fulfill, reject) {
+        try {
+            let request = new XMLHttpRequest();
+            request.open("POST", "/query", true);
+            request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            request.onload = function() {
+                console.log("request" + request);
+                console.log("request.responseText" + request.responseText);
+                let result = request.responseText;
+                console.log(result);
+                if (request.status === 200) {
+                    fulfill(result);
+                } else {
+                    reject(result);
+                }
+
+            };
+            request.send(JSON.stringify(query));
+        } catch (e) {
+            reject(e);
         }
-        return true;
+    });
+}
+
+function jsonParser(name) {
+    //TODO
+    // if (name === "" || name === previousLookupName) return;
+    // let url = "https://api.silveress.ie/bns/v3/character/full/na/" + name;
+    // previousLookupName = name;
+    // showSpinner();
+    //
+    // $.getJSON(url, function(data) {
+    //     hideSpinner();
+    //     if (isEmpty(data)) {
+    //         $("#char-lookup").html("Cannot find you on NA server!");
+    //     } else {
+    //         $("#char-lookup").html(confirmationText(data));
+    //     }
+    //     showCharacterLookup();
+    // });
+}
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
     }
-    
-    function confirmationText(char) {
-        return "LV" + char.playerLevel + " HM" + char.playerLevelHM + " " + char.playerClass + " is that you?";
-    }
-    
-    function showSpinner() {
-        $(".lds-hourglass").css("opacity", "0.8");
-    }
-    
-    function hideSpinner() {
-        $(".lds-hourglass").css("opacity", "0");
-    }
-    
-});
+    return true;
+}
